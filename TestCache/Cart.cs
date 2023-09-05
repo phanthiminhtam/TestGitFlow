5ed031e7c3fd9d51f8b3a37ad0bcb442cec29075
phanthiminhtam <phanminhtam10a2tqk@gmail.com>
2023-09-05 08:28:55 +0700

    anonyatick add cart file

---
+++ TestCache/Cart.cs
@@ -0,0 +1,9 @@
+﻿namespace TestCache
+{
+    public class Cart
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+            public int age { get; set; }
+    }
+}
