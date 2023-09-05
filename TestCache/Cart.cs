2e324c2c5fbb8102d512677e39a8470fa5282e8e
phanthiminhtam <phanminhtam10a2tqk@gmail.com>
2023-09-05 08:50:21 +0700

    #1 - phanminhtam

--- TestCache/Cart.cs
+++ TestCache/Cart.cs
@@ -6,5 +6,9 @@
         public string name { get; set; }
         public int age { get; set; }
         public string NameID { get; set; }
+
+        public string NameName { get; set; }
+        public string jhjh { get; set; }
+        
     }
 }
